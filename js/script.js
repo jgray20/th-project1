8cd51d129cfcfa4b6018fa0baaa73192577381c4
jgray20 <jcstofferahn@gmail.com>
2022-02-19 10:27:36 -0600

    Updates to Quote Generator code

--- js/script.js
+++ js/script.js
@@ -10,21 +10,62 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+let quotes = [
+  {
+    quote: "Design is intelligence made visible.",
+    source: "Alina Wheeler"
+  },
+  {
+    quote: "So many books, so little time.",
+    source: "Frank Zappa"
+  },
+  {
+    quote: "You jump, I jump, Jack.",
+    source: "Rory Gilmore",
+    citation: "Gilmore Girls"
+  },
+  {
+    quote:"I'm not superstitious, but I am a little stitious.",
+    source:"Michael Scott"
+  },
+  {
+    quote:"It does not do to dwell on dreams and forget to live.",
+    source:"Albus Dumbledore",
+    citation: "Harry Potter and the Sorcerer's Stone",
+    year: 1997
+  }
+];
 
 
 /***
  * `getRandomQuote` function
 ***/
 
-
+function getRandomQuote(arr) {
+    let randomNum = Math.floor( Math.random() * arr.length );
+    let randomQuote = arr[randomNum];
+  return randomQuote;
+}
 
 /***
  * `printQuote` function
 ***/
 
-
-
+function printQuote() {
+  let generatedQuote = getRandomQuote(quotes);
+  var html = `
+              <p class="quote"> ${generatedQuote.quote} </p>
+              <p class="source"> ${generatedQuote.source}`;
+              if (generatedQuote.citation) {
+                html += `<span class="citation">${generatedQuote.citation}</span>`;
+              }
+              if (generatedQuote.year) {
+                html += `<span class="year">${generatedQuote.year}</span>`;
+              }
+              html += `</p>`;
+  return html;
+}
+document.getElementById('quote-box').innerHTML = printQuote();
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
